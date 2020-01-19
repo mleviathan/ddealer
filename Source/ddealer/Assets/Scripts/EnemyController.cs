91ffb94442a711c0b1dc68266b9431b5a4545d51
Michele <michele@officinetelematiche.com>
2020-01-19 23:15:44 +0100

    Added jump ability to player, started working on enemy controller

---
+++ Source/ddealer/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float DownTime = 2.0f;
+    public delegate void OnExecuteAction(string action);
+    public OnExecuteAction OnExecute;
+
+    private bool _isJumping = true;
+
+    private void Awake()
+    {
+        OnExecute = ExecuteAction;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.name == "Pavimento" && _isJumping)
+        {
+            _isJumping = false;
+        }
+    }
+
+    private void ExecuteAction(string action)
+    {
+
+    }
+}
--- Source/ddealer/Assets/Scripts/PlayerController.cs
+++ Source/ddealer/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float _jumpForce = 200.0f;
+
+    private bool _isDead = false;
+    private bool _isJumping = true;
+    private Rigidbody2D _playerRigidBody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_isDead)
+            return;
+
+        if (Input.GetMouseButtonDown(0) && !_isJumping)
+        {
+            Jump();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.name == "Floor" && _isJumping)
+        {
+            _isJumping = false;
+        }
+
+        if (collision.collider.gameObject.name == "Obstacle" || collision.collider.gameObject.name == "Enemy")
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        _isDead = true;
+    }
+
+    private void Jump()
+    {
+        _isJumping = true;
+        _playerRigidBody.velocity = Vector2.zero;
+        _playerRigidBody.AddForce(new Vector2(0, _jumpForce));
+
+        FindObjectOfType<EnemyController>().OnExecute("Jump");
     }
 }
