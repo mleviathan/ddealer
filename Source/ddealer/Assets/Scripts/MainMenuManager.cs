fdd7d99d480bb1d806ac70866333a5c32ad43455
Michele Cafagna <mcafagna31@gmail.com>
2020-01-28 21:19:42 +0100

    Added some GUI Elements in Main Menù

--- Source/ddealer/Assets/Scripts/MainMenuManager.cs
+++ Source/ddealer/Assets/Scripts/MainMenuManager.cs
@@ -5,6 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    private const float BLACK_PANELS_FADE_TIME = 1f;
+    private const float MENU_SCALE_TIME = 1.5f;
+
+    [SerializeField]
+    private GameObject _shopMenu;
+    [SerializeField]
+    private GameObject _shopPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +28,26 @@ public class MainMenuManager : MonoBehaviour
     public void Play()
     {
         SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void OpenShop()
+    {
+        _shopMenu.SetActive(true);
+        _shopPanel.SetActive(true);
+        iTween.FadeTo(_shopPanel, 250f, BLACK_PANELS_FADE_TIME);
+        iTween.ScaleTo(_shopMenu, new Vector3(1f, 1f, 1f), MENU_SCALE_TIME);
+    }
+
+    public void CloseShop()
+    {
+        iTween.FadeTo(_shopPanel, 0f, BLACK_PANELS_FADE_TIME);
+        //iTween.ScaleTo(_shopMenu, Vector3.zero, MENU_SCALE_TIME);
+        iTween.ScaleTo(_shopMenu, iTween.Hash("scale", Vector3.zero, "time", MENU_SCALE_TIME, "oncomplete", "DeactivateShop", "oncompletetarget", this.gameObject));
+    }
+
+    public void DeactivateShop()
+    {
+        _shopPanel.SetActive(false);
+        _shopMenu.SetActive(false);
     }
 }
