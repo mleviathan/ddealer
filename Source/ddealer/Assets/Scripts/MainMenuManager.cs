b31e38e9fff16053845db2f87a612a6694d36074
Michele <michele@officinetelematiche.com>
2020-01-31 21:46:29 +0100

    Installed dotween

--- Source/ddealer/Assets/Scripts/MainMenuManager.cs
+++ Source/ddealer/Assets/Scripts/MainMenuManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
@@ -11,7 +12,7 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField]
     private GameObject _shopMenu;
     [SerializeField]
-    private GameObject _shopPanel;
+    private Image _shopPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -33,21 +34,28 @@ public class MainMenuManager : MonoBehaviour
     public void OpenShop()
     {
         _shopMenu.SetActive(true);
-        _shopPanel.SetActive(true);
-        iTween.FadeTo(_shopPanel, 250f, BLACK_PANELS_FADE_TIME);
+        _shopPanel.gameObject.SetActive(true);
         iTween.ScaleTo(_shopMenu, new Vector3(1f, 1f, 1f), MENU_SCALE_TIME);
     }
 
     public void CloseShop()
     {
-        iTween.FadeTo(_shopPanel, 0f, BLACK_PANELS_FADE_TIME);
         //iTween.ScaleTo(_shopMenu, Vector3.zero, MENU_SCALE_TIME);
         iTween.ScaleTo(_shopMenu, iTween.Hash("scale", Vector3.zero, "time", MENU_SCALE_TIME, "oncomplete", "DeactivateShop", "oncompletetarget", this.gameObject));
     }
 
     public void DeactivateShop()
     {
-        _shopPanel.SetActive(false);
+        _shopPanel.gameObject.SetActive(false);
         _shopMenu.SetActive(false);
     }
+
+    IEnumerator FadePanel()
+    {
+        Color actualColor = _shopPanel.color;
+        actualColor.a += BLACK_PANELS_FADE_TIME * Time.deltaTime;
+        _shopPanel.color = actualColor;
+        yield return null;
+    }
+
 }
